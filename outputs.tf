56a77b48313d83fba6cc98075dd038d87190b94a
alewis-code <alewis@lewishasmail.com>
2026-02-02 21:03:49 -0600

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "applied_name" {
+  description = "Returns the name provided in the variables."
+  value       = var.applicant_name
+}
+
+output "applied_position" {
+  description = "Returns the position title provided in the variables."
+  value       = var.position_title
+}
