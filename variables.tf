9e6fb53d7b156893e0a78f9cdcf4bcb296f99b61
alewis-code <alewis@lewishasmail.com>
2026-02-02 21:03:08 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "applicant_name" {
+  type        = string
+  description = "The name of the applicant."
+  default     = "Allen Lewis"
+}
+
+variable "position_title" {
+  type        = string
+  description = "The title of the position being applied for."
+  default     = "Senior DevOps Engineer"
+}
